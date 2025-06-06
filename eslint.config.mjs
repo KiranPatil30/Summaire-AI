8503c25ff19caa002dc336473679507d7b9778e4
Kiran Patil <kiranspatil686@gmail.com>
2025-06-06 12:20:49 +0530

    some chnages in eslint.config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,74 +9,11 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = {
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript","prettier"],
-    plugins:['prettier'],
-    rules:{
-      'prettier/prettier': 'error',
-      'react/no-escape-entities':'off',
-    },
-  }),
-};
-
-export default eslintConfig;
-
-
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// export default [
-//   // Compatibility layer for ESLint plugins/configs that don't yet support flat config
-//   ...compat.extends([
-//     "next/core-web-vitals",
-//     "next",
-//     "prettier",
-//   ]),
-//   {
-//     plugins: {
-//       prettier: require("eslint-plugin-prettier"),
-//     },
-//     rules: {
-//       "prettier/prettier": "error",
-//       "react/no-escape-entities": "off",
-//     },
-//   },
-// ];
-
-
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// export default [
-//   ...compat.extends([
-//     "next/core-web-vitals",
-//     "next",
-//     "prettier",
-//   ]),
-//   {
-//     plugins: {
-//       prettier: require("eslint-plugin-prettier"),
-//     },
-//     rules: {
-//       "prettier/prettier": "error",
-//       "react/no-escape-entities": "off",
-//     },
-//   },
-// ];
+export default compat.config({
+  extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/no-escape-entities": "off",
+  },
+});
